00db1c2e634b4d435d7e77c1a4600abe545341ed
superweb-dev <superweb.dev@yandex.com>
2018-04-07 22:46:04 -0700

    Updated Nav Bar w/ bg

--- resources/views/issues.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.site', ['bodyClass' => ''])
-
-@section ('content')
-    <div class="row expanded small-collapse">
-        <div class="column small-12">
-            <h1>The Issues</h1>
-        </div>
-    </div>
-@stop
