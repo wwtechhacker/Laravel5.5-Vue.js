35d6a2301bef7d5434099bde1f698e72276da542
superweb-dev <superweb.dev@yandex.com>
2018-04-24 18:17:08 -0700

    Added Roster Page

---
+++ resources/views/roster.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.site', ['bodyClass' => 'roster'])
+
+@section ('content')
+    <!-- <header>
+        @include('partials.hero-battles', $hero)
+    </header>
+    <section>
+    <h2 class="text-center">Coming Soon</h2>
+        <div class="row small-up-1 medium-up-3">
+            @foreach($navCards as $cards)
+                <div class="column">
+                    @include('partials.nav-card', $cards)
+                </div>
+            @endforeach
+        </div>
+    </section> -->
+    
+@stop
