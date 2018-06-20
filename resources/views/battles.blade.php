ac7069fc60bed5aef48ccd9228ef6661be3689b8
superweb-dev <superweb.dev@yandex.com>
2018-06-20 01:41:10 -0700

    Temporary battles page video TEMP

--- resources/views/battles.blade.php
+++ resources/views/battles.blade.php
@@ -7,19 +7,19 @@
     <section class="videos">
     <h2 class="text-center">Belly of the Beast IV</h2>
         <div class="row">
-            <div class="column small-12 medium-12">
+            <div class="column small-12 videoWrapper">
             <!-- @foreach($navCards as $cards)
                 <div class="column">
                     @include('partials.nav-card', $cards)
                 </div>
             @endforeach -->
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/RVCFDpQagU0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/I4ysphwrigw" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/j979oIx8v54" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/PhODNdvxL4g" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/hRoCHKOZOQE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/2tOu2m_Iqgo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/ZmMq49irBwY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            <iframe width="100%" height="auto" src="https://www.youtube.com/embed/RVCFDpQagU0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            <!-- <iframe width="100%" height="350" src="https://www.youtube.com/embed/I4ysphwrigw" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            <iframe width="100%" height="315" src="https://www.youtube.com/embed/j979oIx8v54" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            <iframe width="100%" height="315" src="https://www.youtube.com/embed/PhODNdvxL4g" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            <iframe width="100%" height="315" src="https://www.youtube.com/embed/hRoCHKOZOQE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            <iframe width="100%" height="315" src="https://www.youtube.com/embed/2tOu2m_Iqgo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            <iframe width="100%" height="315" src="https://www.youtube.com/embed/ZmMq49irBwY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe> -->
         </div>
     </section>
 @stop
