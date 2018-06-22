923c57c26e9f984894c9ef500968f3d8be6c10f2
superweb-dev <superweb.dev@yandex.com>
2018-06-21 21:35:27 -0700

    merge develop

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -2,11 +2,7 @@
     <div class="title-bar-left">
         <div class="logo">
             <a class="ga-logo" href="/" data-ga-category="Navigational Clicks" data-ga-action="Click Header" data-ga-label="Logo">
-<<<<<<< HEAD
-                <img src="/images/blank.png" data-interchange="[{{ asset('images/shotsFired_logoText.png') }}, small]"alt="site logo">
-=======
                 <img src="/images/blank.png" data-interchange="[{{ asset('images/shotsFired_logoText.png') }}, small], [{{ asset('images/shotsFired_logoText.png') }}, medium]" alt="site logo">
->>>>>>> develop
             </a>
         </div>
     </div>
