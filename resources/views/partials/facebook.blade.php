34d80a86cd010cfea09c07d64589fda83303e0c6
superweb-dev <superweb.dev@yandex.com>
2018-09-29 03:05:08 -0700

    updated fb puglin

--- resources/views/partials/facebook.blade.php
+++ resources/views/partials/facebook.blade.php
@@ -7,7 +7,7 @@
             xfbml           : true,
             version         : 'v2.9'
         });
-//        FB.AppEvents.logPageView();
+        FB.AppEvents.logPageView();
     };
 
     (function (d, s, id) {
@@ -19,4 +19,14 @@
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
 </script>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
 @endif
