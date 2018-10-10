b14ba4ccbfb0133c03859db7a534f6655457415e
superweb-dev <superweb.dev@yandex.com>
2018-10-09 23:57:36 -0700

    updated laravel route

--- routes/web.php
+++ routes/web.php
@@ -50,3 +50,7 @@ Route::resource('/api/markers', 'MarkerController');
 // Logout route
 Route::get('/admin/logout', '\App\Http\Controllers\Auth\LoginController@logout');
 
+Route::get('/{vue_capture?}', function () {
+    return view('battles');
+   })->where('vue_capture', '[\/\w\.-]*');
+
